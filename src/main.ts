6cb11ed54349fc971040c805868c3789f5a2efdc
Ayodeji Obasan <dejiobasan@yahoo.com>
2025-06-25 10:29:04 +0100

    Added Swagger UI for Api documentation.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder() // This is used to configure the Swagger documentation.
+    .setTitle('Finance Tracker API')
+    .setDescription('API documentation for the Finance Tracker')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
